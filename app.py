9c3be09051d00ab19eb9578d803259f7cf931f71
Seth Raphael <setharaphael7@gmail.com>
2021-06-15 20:41:26 -0700

    added navigation between lessons

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask, make_response, jsonify, request, render_template, redirect, send_file
 from mistune import Markdown
-import json
+import json, os, dotenv
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+dotenv.load_dotenv()
+if os.environ.get("MONGO_URI", None):
+    app.config['MONGO_URI'] = os.environ.get("MONGO_URI", None)
+    mongo = PyMongo(app)
+else:
+    mongo = None
 markdown = Markdown()
 
 
@@ -18,7 +25,10 @@ def compile_code():
 
 @app.route("/lesson", methods=["GET", "POST"])
 def lesson():
-    return markdown(open(f"lessons/{request.args.get('name')}.md").read())
+    content = markdown(open(f"lessons/{request.args.get('name')}.md").read())
+    code = '\n'.join(json.load(open("resources/code.json"))[request.args.get('name')])
+    expected_output = json.load(open("resources/expected_output.json"))[request.args.get('name')]
+    return jsonify({"lesson": content, "code": code, "expected_output": expected_output, "name": request.args.get('name')})
 
 
 if __name__ == '__main__':
--- static/compile.js
+++ static/compile.js
@@ -1,47 +1,67 @@
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms))
 }
-
-let codeArea = CodeMirror(document.getElementById("right"), {
-            value: code,
-            mode:  "python",
-            lineNumbers: true,
-            theme: "theme",
-            indentUnit: 4
-        });
-let spacer = document.createElement("div")
-spacer.id = "spacer"
-let runButton = document.createElement("button")
-runButton.addEventListener("click", () => {run()})
-runButton.id = "run"
-runButton.classList.add("run")
-runButton.innerText = "Run ❯"
-let clearButton = document.createElement("button")
-clearButton.addEventListener("click", () => clear())
-clearButton.id = "clear"
-clearButton.classList.add("run")
-clearButton.innerText = "Clear"
-document.getElementById("right").appendChild(runButton)
-document.getElementById("right").appendChild(spacer)
+// Initialize variables
+let codeArea;
+let expected_output;
 let output;
-output = createShell('>>> ')
-document.body.appendChild(clearButton)
-codeArea.setSize(window.innerWidth/2, window.innerHeight/1.7)
+const lessons_to_nums = {"intro": 0, "variables": 1, "strings": 2, "builtins": 3, "ints_and_floats": 4,
+    "math": 5, "booleans": 6, "if_statements": 7, "lists": 8, "dictionaries": 9, "functions": 10}
+const nums_to_lessons = {0: 'intro', 1: 'variables', 2: 'strings', 3: 'builtins', 4: 'ints_and_floats',
+    5: 'math', 6: 'booleans', 7: 'if_statements', 8: 'lists', 9: 'dictionaries', 10: 'functions'}
+const responses = ["Great job!", "Good work!", "Great work!", "Good work!", "Nicely done!", "Nice job!", "Nice work!", "Well done!"]
+
+
+// Create the main function
+async function main() {
+    const response = await (await fetch(`/lesson?name=${localStorage.getItem('lesson')}`)).json()
+    console.log(response)
+    document.getElementById("left").innerHTML = response['lesson'] + `<div>${" f".repeat(50)}</div>`
+    expected_output = response['expected_output']
+    codeArea = CodeMirror(document.getElementById("right"), {
+        value: response['code'],
+        mode: "python",
+        lineNumbers: true,
+        theme: "theme",
+        indentUnit: 4
+    });
+    console.log(response['name'])
+    if (response['name'] === "intro") {document.getElementById("back").classList.add("invalid")}
+    let spacer = document.createElement("div")
+    spacer.id = "spacer"
+    let runButton = document.createElement("button")
+    runButton.addEventListener("click", () => run())
+    runButton.id = "run"
+    runButton.classList.add("run")
+    runButton.innerText = "Run ❯"
+    let clearButton = document.createElement("button")
+    clearButton.addEventListener("click", () => clear())
+    clearButton.id = "clear"
+    clearButton.classList.add("run")
+    clearButton.innerText = "Clear"
+    document.getElementById("right").appendChild(runButton)
+    document.getElementById("right").appendChild(spacer)
+    output = createShell('>>> ')
+    document.body.appendChild(clearButton)
+    codeArea.setSize(window.innerWidth / 2, window.innerHeight / 1.7)
+}
+
+
 async function run() {
     let code = codeArea.getValue()
     console.log(code)
     let results = await fetch("https://emkc.org/api/v2/piston/execute", {
         method: 'POST',
         body: JSON.stringify({"language": "python", "version": "3.9.4", "files": [{"name": "code.py", "content": code}]})})
-
     console.log(results)
     results = await results.json()
     console.log(results)
     let previousOutput = output.getValue()
     output = createShell(previousOutput+'\n'+results['run']['stdout']+results['run']['stderr']+'\n>>> ')
     document.getElementById("right").children[3].remove()
-    checkResults(results['run'])
+    await checkResults(results['run'])
 }
+
 
 function createShell(value) {
     output = CodeMirror(document.getElementById("right"), {
@@ -61,6 +81,7 @@ function clear() {
     createShell('>>> ')
     document.getElementById("right").children[3].remove()
 }
+
 
 function keyBind(editor) {
     editor.setOption("extraKeys", {
@@ -82,6 +103,7 @@ function keyBind(editor) {
     })
 }
 
+
 async function checkResults(results) {
     let regex = new RegExp(expected_output)
     console.log(expected_output)
@@ -96,11 +118,11 @@ async function checkResults(results) {
         return await send_notif("incorrect", `Incorrect output: \n${results['stdout']}\nTry again!`)
     }
     console.log("Correct")
-    let responses = ["Great job!", "Good work!", "Great work!", "Good work!", "Nicely done!", "Nice job!", "Nice work!", "Well done!"]
     let response = responses[Math.floor(Math.random()*responses.length)];
     document.getElementById("next").classList.add("valid")
     return await send_notif("correct", `${response} Click Next to continue!`)
 }
+
 
 async function send_notif(status, text) {
     let notif = document.getElementById("notif")
@@ -129,3 +151,25 @@ async function send_notif(status, text) {
     }
     notif.style.display = "none"
 }
+
+
+function nextLesson(prev) {
+    let lessonNum;
+    if (prev) {
+        lessonNum = lessons_to_nums[localStorage.getItem('lesson')]-1
+        if (document.getElementById("back").classList.contains("invalid")) {return}
+    }
+    else {
+        lessonNum = lessons_to_nums[localStorage.getItem('lesson')]+1
+        if (!document.getElementById("next").classList.contains("valid")) {return}
+    }
+    let newLesson = nums_to_lessons[lessonNum]
+    localStorage.setItem("lesson", newLesson)
+    location.reload()
+}
+
+// When the page first loads, make the code check if there is already the key "lesson" in localstorage.
+// If not, it creates one `"lesson": "intro"`
+// It then passes it to the endpoint in the python
+// When the user signs up for an account, it checks the localstorage to see if they have made progress.
+// If they have, instead of setting their current lesson to "intro", it sets it to the one in localstorage.
